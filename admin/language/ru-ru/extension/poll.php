a686a66a65932398a36c7cee3c316bd0f23f4505
Roman Dubs <drova.tm@gmail.com>
2018-06-11 16:27:52 +0300

    первая заливка

---
+++ admin/language/ru-ru/extension/poll.php
@@ -0,0 +1,39 @@
+<?php
+// Heading 
+$_['heading_title']     		= 'Редактор голосований';
+$_['text_list']     		    = 'Все голосования';
+$_['text_answer']     		    = 'Варианты ответов';
+
+
+// Text
+$_['entry_name']                = 'Заголовок';
+$_['entry_date_added']          = 'Дата добавления';
+
+$_['button_add_answer']         = 'Добавить ответ';
+$_['button_del_answer']         = 'Удалить ответ';
+$_['button_filter']             = 'Фильтр голосований';
+$_['button_view_all_poll']      = 'Смотреть все голосования';
+$_['button_copy']      			= 'Copy';
+
+$_['text_edit']                 = 'Редактировать'; 
+$_['text_image']				= 'Картинка';
+$_['text_title']				= 'Заголовок';
+$_['text_name']	            	= 'Название голосования';
+$_['text_description']		    = 'Описание голосования';
+$_['text_date']					= 'Дата добавления';
+$_['text_date_start']			= 'Дата начала голосования';
+$_['text_date_end']				= 'Дата окончания голосования';
+$_['text_action']				= 'Действие';
+$_['text_votes']				= 'Голосов';
+$_['text_status']				= 'Статус';
+$_['text_sort_order']			= 'Сортировка';
+$_['text_keyword']				= 'SEO Keyword';
+$_['text_no_results']			= 'Пусто!';
+$_['text_browse']				= 'Browse';
+$_['text_clear']				= 'Очистить';
+$_['text_image_manager']		= 'Менеджер изображений';
+$_['text_success']				= 'Вы успешно изменили голосование!';
+
+// Error
+$_['error_permission'] 			= 'Warning: У вас нет прав для редактирования голосования!';
+?>
